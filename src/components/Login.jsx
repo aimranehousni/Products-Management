17734f1fb687d334f83536b98cf3825b7beecdb8
Aimrane <hooaimrane@gmail.com>
2025-04-01 20:56:54 +0000

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { login } from '../features/usersSlice';
-import "../styles/login.css";
+import "@styles/login.css"
 
 const Login = () => {
     const [username, setUsername] = useState('');
